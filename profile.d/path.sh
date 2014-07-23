9495f9059c029f5dafe16a17f43b6f7f9f9524fa
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-07-23 16:30:33 +0200

    path: remove unneeded paths on the mac
    That is to be checked again on ubuntu. I have pyenv and rbenv installed
    via brew on the mac, and cloned to ~/xxenv on ubuntu.

--- profile.d/path.sh
+++ profile.d/path.sh
@@ -1,1 +1,4 @@
-export PATH="$PYENV_ROOT/bin:$HOME/.rbenv/bin:~/bin:$PATH:/usr/texbin"
+# on mac, no need to add those. To check again on ubuntu.
+# export PATH="$PYENV_ROOT/bin:$HOME/.rbenv/bin:$PATH:/usr/texbin"
+
+export PATH="~/bin:$PATH"
