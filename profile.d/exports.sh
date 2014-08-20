d145ed8cec187d2f3d6ed2405e7ea443f06be020
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-08-20 14:17:10 +0200

    do not set TERM on bash_profile on ubuntu
    Because it is executed inside tmux sessions and we don't want to
    override the screen_256color setting

--- profile.d/exports.sh
+++ profile.d/exports.sh
@@ -2,9 +2,5 @@ export LESS=-FXRSq
 
 export PYENV_ROOT="$HOME/.pyenv"
 
-if [ "$(uname)" == "Linux" ]; then
-        export TERM=xterm-256color
-fi
-
 export HISTCONTROL=erasedups
 export HISTSIZE=10000
