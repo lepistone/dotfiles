e95a849ba69de81c6e3b21fbc8c5a06282b13bd3
Leonardo Pistone <leonardo.pistone@camptocamp.com>
2014-08-26 14:02:56 +0200

    ~/bin before linuxbrew in PATH
    I need this to get git-remote-bzr right

--- profile.d/path.sh
+++ profile.d/path.sh
@@ -1,9 +1,9 @@
 # on mac, no need to add those. To check again on ubuntu.
 # export PATH="$PYENV_ROOT/bin:$HOME/.rbenv/bin:$PATH:/usr/texbin"
 
-export PATH="~/bin:$PATH"
-
 if [ -d ~/.linuxbrew/bin ]
 then
   export PATH="$HOME/.linuxbrew/bin:$PATH"
 fi
+
+export PATH="~/bin:$PATH"
