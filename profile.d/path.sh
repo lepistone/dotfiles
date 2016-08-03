bb41103c5d9a374ede4cf28aca2d9b78f389bd4c
Leonardo Pistone <lepistone@spotify.com>
2016-08-03 10:52:38 +0200

    put /usr/local first, otherwise I get system pip

--- profile.d/path.sh
+++ profile.d/path.sh
@@ -6,7 +6,7 @@ then
 fi
 
 if [ -d ~/Library/Python/2.7/bin ]; then
-  export PATH="$HOME/Library/Python/2.7/bin:$PATH"
+  export PATH="$PATH:$HOME/Library/Python/2.7/bin"
 fi
 
 export PATH="$HOME/bin:/usr/local/sbin:$PATH"
