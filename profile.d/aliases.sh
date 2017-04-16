3e5fb192fd9988de27e22338630563c1ba43e739
Leonardo Pistone <lepistone@spotify.com>
2017-04-16 12:24:32 +0200

    remove stuff I don't use anymore

--- profile.d/aliases.sh
+++ profile.d/aliases.sh
@@ -4,13 +4,8 @@ alias g='git'
 
 alias ls='ls -pG'
 
-alias lockscreen='/System/Library/CoreServices/Menu\ Extras/User.menu/Contents/Resources/CGSession -suspend'
-
-alias gpg='gpg2'
-
 alias doma='docker-machine'
 alias doco='docker-compose'
-alias bro='google-chrome --incognito $(docker-compose port odoo 8069)'
 
 alias goodssl='/usr/local/opt/openssl/bin/openssl'
 alias libressl='/usr/local/opt/libressl/bin/openssl'
--- profile.d/path.sh
+++ profile.d/path.sh
@@ -1,22 +1,5 @@
-# on mac, no need to add those. To check again on ubuntu.
-
-if [ -d ~/.linuxbrew/bin ]
-then
-  export PATH="$HOME/.linuxbrew/sbin:$HOME/.linuxbrew/bin:$PATH"
-fi
-
 if [ -d ~/Library/Python/2.7/bin ]; then
   export PATH="$PATH:$HOME/Library/Python/2.7/bin"
 fi
 
 export PATH="$HOME/bin:/usr/local/sbin:$PATH"
-
-if [ -d /usr/local/texlive/2015basic/bin/x86_64-darwin ]
-then
-  export PATH=/usr/local/texlive/2015basic/bin/x86_64-darwin:$PATH
-fi
-
-if [ -d ~/opt/idea ]
-then
-  export PATH=~/opt/idea/bin:$PATH
-fi
