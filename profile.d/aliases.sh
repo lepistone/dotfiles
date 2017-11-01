baa6aec1ec3978ce29c44500c5598482a6a6a4af
Leonardo Pistone <lepistone@spotify.com>
2017-11-02 00:19:31 +0100

    select java version

--- profile.d/aliases.sh
+++ profile.d/aliases.sh
@@ -16,3 +16,7 @@ alias an='ansible'
 alias anp='ansible-playbook'
 
 alias bse='brew services'
+
+alias java7='export JAVA_HOME=$(/usr/libexec/java_home -F -v 1.7); java -version'
+alias java8='export JAVA_HOME=$(/usr/libexec/java_home -F -v 1.8); java -version'
+alias java9='export JAVA_HOME=$(/usr/libexec/java_home -F -v 9); java -version'
