ef5ba887769f2355be49aa0e06b862c79ee9bcbb
Leonardo Pistone <lepistone@spotify.com>
2017-11-04 18:54:23 +0100

    fzf: find only files

--- profile.d/exports.sh
+++ profile.d/exports.sh
@@ -2,4 +2,4 @@ export LESS=-FXRSq
 export PYTHONSTARTUP=~/.pystartup
 export EDITOR=vim
 export GPG_TTY=$(tty)
-export FZF_DEFAULT_COMMAND=fd
+export FZF_DEFAULT_COMMAND='fd --type f'  # fast, respects gitignore
