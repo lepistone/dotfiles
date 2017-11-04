c2c3ebb4942d6e202f4fbc34d0658662a892c2b2
Leonardo Pistone <lepistone@spotify.com>
2017-11-04 18:58:42 +0100

    find hidden except .git, same on C-t

--- profile.d/exports.sh
+++ profile.d/exports.sh
@@ -2,4 +2,5 @@ export LESS=-FXRSq
 export PYTHONSTARTUP=~/.pystartup
 export EDITOR=vim
 export GPG_TTY=$(tty)
-export FZF_DEFAULT_COMMAND='fd --type f'  # fast, respects gitignore
+export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
+export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
