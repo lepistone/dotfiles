871069b54779d4756495a485480e76d249f7dfee
Leonardo Pistone <lepistone@spotify.com>
2017-11-30 13:44:30 +0100

    Fix gcloud error: it expects system python

--- profile.d/exports.sh
+++ profile.d/exports.sh
@@ -4,3 +4,4 @@ export EDITOR=vim
 export GPG_TTY=$(tty)
 export FZF_DEFAULT_COMMAND='fd --type f --hidden --exclude .git'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+export CLOUDSDK_PYTHON=python
